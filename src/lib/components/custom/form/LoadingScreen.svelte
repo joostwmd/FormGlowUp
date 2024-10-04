4c7705e95e07c65dadf09fb19ba6dbdc978f48c1
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-04 18:47:15 +0200

    Add LoadingScreen component with Loader

---
+++ src/lib/components/custom/form/LoadingScreen.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Loader from '../Loader.svelte';
+
+	export let variant: string;
+</script>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	<Loader {variant} />
+</div>
