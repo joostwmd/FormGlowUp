751347a867ec0da7909a3840ed9d1e8db2b25c19
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-09 15:41:42 +0200

    end screen text uses store

--- src/lib/components/custom/EndScreenCustomizer.svelte
+++ src/lib/components/custom/EndScreenCustomizer.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Input from '../shadcn/ui/input/input.svelte';
-
-	let loacelTestValue = 'Leave a nice message for the people that filled out the survey';
+	import { formStructureStore } from '$lib/form/stores';
 </script>
 
 <div class="flex flex-col space-y-4 md:space-y-6">
@@ -10,6 +9,6 @@
 	</div>
 
 	<div class="w-full px-4">
-		<Input bind:value={loacelTestValue} />
+		<Input bind:value={$formStructureStore.endText} />
 	</div>
 </div>
