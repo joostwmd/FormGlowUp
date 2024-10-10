e24f703c0b9067b27f4e5a64fc92b1f57261f299
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-10 14:37:49 +0200

    nide nav

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
 	import Nav from '$lib/components/custom/Nav.svelte';
+	import type { LayoutServerData } from './$types';
 	import '../../app.css';
+	import { page } from '$app/stores';
+	export let data: LayoutServerData;
+
+	const hideNavRoutes = ['/public', '/preview'];
+	let navVisible: boolean = false;
+
+	$: {
+		const mode = $page.params.mode;
+		if (hideNavRoutes.includes(mode)) {
+			navVisible = false;
+		} else {
+			navVisible = true;
+		}
+	}
 </script>
 
+{#if navVisible}
+	<Nav />
+{/if}
 <div class="h-screen w-screen bg-muted/40 p-4">
 	<slot />
 </div>
