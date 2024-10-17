50764c481b731d6d036301951246e49012b3b3b7
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-17 18:10:05 +0200

    paragrpah input

---
+++ src/lib/components/custom/form/ParagraphInput.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Textarea } from '$lib/components/shadcn/ui/textarea';
+	import { SUBMIT_KEY_PREFIX } from '$lib/form/constants';
+	import { formDataStore } from '$lib/form/stores';
+	import type { TTextItem } from '$lib/form/types';
+	import { handleFormValueChange } from '$lib/form/utils/client';
+
+	export let item: TTextItem;
+</script>
+
+<h1>text</h1>
+<Textarea
+	value={$formDataStore[SUBMIT_KEY_PREFIX + item.submitId]}
+	on:change={(e) => handleFormValueChange(e.target.value, item.submitId)}
+/>
