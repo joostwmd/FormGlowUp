08dc4469e375c5fb629679bbbdc1ea9306c58068
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-17 18:07:29 +0200

    Refactor TextInput component to use typed parameters and update form value handling

--- src/lib/components/custom/form/TextInput.svelte
+++ src/lib/components/custom/form/TextInput.svelte
@@ -2,14 +2,14 @@
 	import { Input } from '$lib/components/shadcn/ui/input';
 	import { SUBMIT_KEY_PREFIX } from '$lib/form/constants';
 	import { formDataStore } from '$lib/form/stores';
+	import type { TTextItem } from '$lib/form/types';
 	import { handleFormValueChange } from '$lib/form/utils/client';
 
-	export let submitId: string;
-	export let description: string | null = null;
+	export let item: TTextItem;
 </script>
 
 <h1>text</h1>
 <Input
-	value={$formDataStore[SUBMIT_KEY_PREFIX + submitId]}
-	on:change={(e) => handleFormValueChange(e.target.value, submitId)}
+	value={$formDataStore[SUBMIT_KEY_PREFIX + item.submitId]}
+	on:change={(e) => handleFormValueChange(e.target.value, item.submitId)}
 />
