3dc65b026ea62668bc75cfd44e4cef4c944a02fd
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-17 18:05:09 +0200

    Refactor CheckboxGroup component to use typed parameters and update form value handling

--- src/lib/components/custom/form/CheckboxGroup.svelte
+++ src/lib/components/custom/form/CheckboxGroup.svelte
@@ -8,32 +8,30 @@
 		SUBMIT_KEY_PREFIX
 	} from '$lib/form/constants';
 	import { formDataStore } from '$lib/form/stores';
+	import type { TChoicesItem } from '$lib/form/types';
 	import { handleFormValueChange } from '$lib/form/utils/client';
 
-	export let description: string | null = null;
-	export let randomizeOrder: boolean = false;
-	export let options: string[];
-	export let submitId: string;
+	export let item: TChoicesItem;
 
 	function handleCheckboxChange(option: string) {
-		const currentValues = $formDataStore[`${SUBMIT_KEY_PREFIX}${submitId}`] || [];
+		const currentValues = $formDataStore[`${SUBMIT_KEY_PREFIX}${item.submitId}`] || [];
 		let updatedValues = [];
 
 		if (currentValues.includes(option)) {
-			updatedValues = currentValues.filter((value) => value !== option);
+			updatedValues = currentValues.filter((value: string) => value !== option);
 		} else {
 			updatedValues = [...currentValues, option];
 		}
 
-		handleFormValueChange(updatedValues, submitId);
+		handleFormValueChange(updatedValues, item.submitId);
 	}
 
 	let otherResponseEnabled: boolean = false;
 
 	$: {
-		if ($formDataStore[`${SUBMIT_KEY_PREFIX}${submitId}`]) {
+		if ($formDataStore[`${SUBMIT_KEY_PREFIX}${item.submitId}`]) {
 			otherResponseEnabled =
-				$formDataStore[`${SUBMIT_KEY_PREFIX}${submitId}`].includes(OTHER_OPTION_VALUE);
+				$formDataStore[`${SUBMIT_KEY_PREFIX}${item.submitId}`].includes(OTHER_OPTION_VALUE);
 		} else {
 			otherResponseEnabled = false;
 		}
@@ -41,12 +39,12 @@
 </script>
 
 <div class="w-full">
-	{#if description}
-		<p class="text-sm text-gray-500">{description}</p>
+	{#if item.displayData.description}
+		<p class="text-sm text-gray-500">{item.displayData.description}</p>
 	{/if}
 
 	<div>
-		{#each options as option}
+		{#each item.options as option}
 			<div class="mb-2 flex flex h-8 items-center space-x-2">
 				{#if option === OTHER_OPTION_VALUE}
 					<Checkbox onCheckedChange={() => handleCheckboxChange(OTHER_OPTION_VALUE)} />
@@ -54,7 +52,9 @@
 						type="text"
 						class="p-2"
 						disabled={!otherResponseEnabled}
-						bind:value={$formDataStore[`${SUBMIT_KEY_PREFIX}${submitId}${OTHER_RESPONSE_SUFFIX}`]}
+						bind:value={$formDataStore[
+							`${SUBMIT_KEY_PREFIX}${item.submitId}${OTHER_RESPONSE_SUFFIX}`
+						]}
 					/>
 				{:else}
 					<Checkbox onCheckedChange={() => handleCheckboxChange(option)} />
