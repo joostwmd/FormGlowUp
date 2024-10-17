8eed4502aeb49a68e1a7b1f5d90e14ed2c375582
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-17 18:06:49 +0200

    Refactor SliderInput component to use typed parameters and update form value handling

--- src/lib/components/custom/form/SliderInput.svelte
+++ src/lib/components/custom/form/SliderInput.svelte
@@ -2,34 +2,30 @@
 	import { SUBMIT_KEY_PREFIX } from '$lib/form/constants';
 	import { formDataStore } from '$lib/form/stores';
 	import Slider from '$lib/components/shadcn/ui/slider/slider.svelte';
+	import type { TScaleItem } from '$lib/form/types';
 
-	export let description: string | null = null;
-	export let minValue: number;
-	export let maxValue: number;
-	export let minLabel: string | null = null;
-	export let maxLabel: string | null = null;
-	export let submitId: string;
+	export let item: TScaleItem;
 </script>
 
 <div class="flex w-full flex-col items-start space-y-4">
-	{#if description}
-		<p class="text-sm text-gray-500">{description}</p>
+	{#if item.displayData.description}
+		<p class="text-sm text-gray-500">{item.displayData.description}</p>
 	{/if}
 
 	<div class="flex w-full max-w-96 flex-col">
 		<div class="mb-4 flex w-full justify-between">
-			{#if minLabel}
-				<p class="text-xs text-gray-500">{minLabel}</p>
+			{#if item.attributes.minLabel}
+				<p class="text-xs text-gray-500">{item.attributes.minLabel}</p>
 			{/if}
-			{#if maxLabel}
-				<p class="text-xs text-gray-500">{maxLabel}</p>
+			{#if item.attributes.maxLabel}
+				<p class="text-xs text-gray-500">{item.attributes.maxLabel}</p>
 			{/if}
 		</div>
 		<Slider
 			class="bg-secondary"
-			bind:value={$formDataStore[`${SUBMIT_KEY_PREFIX}${submitId}`]}
-			min={minValue}
-			max={maxValue}
+			bind:value={$formDataStore[`${SUBMIT_KEY_PREFIX}${item.submitId}`]}
+			min={item.attributes.min}
+			max={item.attributes.max}
 		/>
 	</div>
 </div>
