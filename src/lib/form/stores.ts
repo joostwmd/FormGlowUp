ed7cf8f91ddfd5e1c20f1468ec0ac59146377717
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-18 17:23:17 +0200

    removed unused code (old stored)

--- src/lib/form/stores.ts
+++ src/lib/form/stores.ts
@@ -1,66 +1,12 @@
 import { writable } from 'svelte/store';
 import type { TFormInfo, TFormItem, TFormPages, TFormStyle } from './types';
 
-// export type TFormStyleOld = {
-// 	theme: string;
-// 	radius: number;
-// 	font: string;
-// 	mode: 'light' | 'dark';
-// };
-
-// export const formStyleStore = writable<TFormStyle>();
-
-// export type TFormStrucutre = {
-// 	loader: string;
-// 	endText: string;
-// 	questions: {
-// 		type: string;
-// 		title: string | null;
-// 		data: { submitId: string; [key: string]: any };
-// 	}[];
-// };
-
-// export const formStructureStore = writable<TFormStrucutre>();
-
-// export type TFormInfoStore = {
-// 	description: string;
-// 	documentTitle: string;
-// 	formId: string;
-// 	responderUri: string;
-// 	title: string;
-// };
-
-// export const formInfoStore = writable<TFormInfoStore>();
-
 //for submit
 export type TFormDataStore = {
-	[key: string]: any;
+	[key: string]: string | number | null;
 };
 
 export const formDataStore = writable<TFormDataStore>({});
-
-// export type TFormState = {
-// 	formInfo: TFormInfoStore;
-// 	formStyle: TFormStyleOld;
-// 	formStructure: string;
-// };
-
-// export const formStateStore = writable<TFormState>({
-// 	formInfo: {
-// 		description: '',
-// 		documentTitle: '',
-// 		formId: '',
-// 		responderUri: '',
-// 		title: ''
-// 	},
-// 	formStyle: {
-// 		theme: '',
-// 		radius: 0,
-// 		font: '',
-// 		mode: 'light'
-// 	},
-// 	formStructure: ''
-// });
 
 export type TFormStore = {
 	info: TFormInfo;
@@ -96,3 +42,26 @@ export const formStore = writable<TFormStore>({
 		}
 	}
 });
+
+// export type TFormState = {
+// 	formInfo: TFormInfoStore;
+// 	formStyle: TFormStyleOld;
+// 	formStructure: string;
+// };
+
+// export const formStateStore = writable<TFormState>({
+// 	formInfo: {
+// 		description: '',
+// 		documentTitle: '',
+// 		formId: '',
+// 		responderUri: '',
+// 		title: ''
+// 	},
+// 	formStyle: {
+// 		theme: '',
+// 		radius: 0,
+// 		font: '',
+// 		mode: 'light'
+// 	},
+// 	formStructure: ''
+// });
