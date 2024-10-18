3a768c2355d00779cdc93ccffbe6c4a64f35549e
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-10-18 16:00:22 +0200

    removed unused stores

--- src/lib/form/stores.ts
+++ src/lib/form/stores.ts
@@ -1,36 +1,36 @@
 import { writable } from 'svelte/store';
 import type { TFormInfo, TFormItem, TFormPages, TFormStyle } from './types';
 
-export type TFormStyleOld = {
-	theme: string;
-	radius: number;
-	font: string;
-	mode: 'light' | 'dark';
-};
+// export type TFormStyleOld = {
+// 	theme: string;
+// 	radius: number;
+// 	font: string;
+// 	mode: 'light' | 'dark';
+// };
 
-export const formStyleStore = writable<TFormStyle>();
+// export const formStyleStore = writable<TFormStyle>();
 
-export type TFormStrucutre = {
-	loader: string;
-	endText: string;
-	questions: {
-		type: string;
-		title: string | null;
-		data: { submitId: string; [key: string]: any };
-	}[];
-};
+// export type TFormStrucutre = {
+// 	loader: string;
+// 	endText: string;
+// 	questions: {
+// 		type: string;
+// 		title: string | null;
+// 		data: { submitId: string; [key: string]: any };
+// 	}[];
+// };
 
-export const formStructureStore = writable<TFormStrucutre>();
+// export const formStructureStore = writable<TFormStrucutre>();
 
-export type TFormInfoStore = {
-	description: string;
-	documentTitle: string;
-	formId: string;
-	responderUri: string;
-	title: string;
-};
+// export type TFormInfoStore = {
+// 	description: string;
+// 	documentTitle: string;
+// 	formId: string;
+// 	responderUri: string;
+// 	title: string;
+// };
 
-export const formInfoStore = writable<TFormInfoStore>();
+// export const formInfoStore = writable<TFormInfoStore>();
 
 //for submit
 export type TFormDataStore = {
@@ -39,28 +39,28 @@ export type TFormDataStore = {
 
 export const formDataStore = writable<TFormDataStore>({});
 
-export type TFormState = {
-	formInfo: TFormInfoStore;
-	formStyle: TFormStyleOld;
-	formStructure: string;
-};
+// export type TFormState = {
+// 	formInfo: TFormInfoStore;
+// 	formStyle: TFormStyleOld;
+// 	formStructure: string;
+// };
 
-export const formStateStore = writable<TFormState>({
-	formInfo: {
-		description: '',
-		documentTitle: '',
-		formId: '',
-		responderUri: '',
-		title: ''
-	},
-	formStyle: {
-		theme: '',
-		radius: 0,
-		font: '',
-		mode: 'light'
-	},
-	formStructure: ''
-});
+// export const formStateStore = writable<TFormState>({
+// 	formInfo: {
+// 		description: '',
+// 		documentTitle: '',
+// 		formId: '',
+// 		responderUri: '',
+// 		title: ''
+// 	},
+// 	formStyle: {
+// 		theme: '',
+// 		radius: 0,
+// 		font: '',
+// 		mode: 'light'
+// 	},
+// 	formStructure: ''
+// });
 
 export type TFormStore = {
 	info: TFormInfo;
