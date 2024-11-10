6803917a43b0bb4d90bdcb988ff4059243d41bfc
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-10 17:20:45 +0100

    Refactor UpdateFormButton component to track and display changed categories

--- src/lib/components/custom/UpdateFormButton.svelte
+++ src/lib/components/custom/UpdateFormButton.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { formStore } from '$lib/form/stores';
+	import { localFormStore, dbFormStore } from '$lib/form/stores';
 	import * as Tooltip from '$lib/components/shadcn/ui/tooltip/index.js';
 	import { Button } from '$lib/components/shadcn/ui/button/index';
 	import SaveIcon from 'lucide-svelte/icons/arrow-down-to-line';
@@ -28,44 +28,61 @@
 
 	export let isUpdating: boolean = false;
 	let needsUpdate: boolean = false;
+	let changedCategories: string[] = [];
 
 	$: {
-		const savedFormState = $formStore;
-		const localFormInfo = $formStore.info;
-		const localFormItems = $formStore.items;
-		const localFormStyle = $formStore.style;
+		const dbFormState = $dbFormStore;
+		const localFormState = $localFormStore;
 
-		const formInfoChanged = !deepEqual(savedFormState.info, localFormInfo);
-		const formItemsChanged = !deepEqual(savedFormState.items, localFormItems);
-		const formStyleChanged = !deepEqual(savedFormState.style, localFormStyle);
+		changedCategories = [];
 
-		if (formInfoChanged || formItemsChanged || formStyleChanged) {
-			needsUpdate = true;
-		} else {
-			needsUpdate = false;
-		}
+		const formInfoChanged = !deepEqual(dbFormState.info, localFormState.info);
+		const formItemsChanged = !deepEqual(dbFormState.items, localFormState.items);
+		const formStyleChanged = !deepEqual(dbFormState.style, localFormState.style);
+		const formPagesChanged = !deepEqual(dbFormState.pages, localFormState.pages);
+
+		if (formInfoChanged) changedCategories.push('Info');
+		if (formItemsChanged) changedCategories.push('Items');
+		if (formStyleChanged) changedCategories.push('Style');
+		if (formPagesChanged) changedCategories.push('Pages');
+
+		needsUpdate = changedCategories.length > 0;
 	}
 </script>
 
 <Tooltip.Root>
 	<Tooltip.Trigger>
-		<Button
-			disabled={!needsUpdate || isUpdating}
-			type="submit"
-			variant={needsUpdate ? 'outline' : 'ghost'}
-		>
-			{#if isUpdating}
-				<LoaderCircle class="mr-1 h-4 w-4 animate-spin" />
-				Please Wait
-			{:else}
-				<SaveIcon class="mr-1 h-4 w-4" />
-				Update
+		<div class="relative">
+			{#if needsUpdate}
+				<div
+					class="absolute -left-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-xs text-white"
+				>
+					{changedCategories.length}
+				</div>
 			{/if}
-		</Button>
+			<Button
+				disabled={!needsUpdate || isUpdating}
+				type="submit"
+				variant={needsUpdate ? 'outline' : 'ghost'}
+			>
+				{#if isUpdating}
+					<LoaderCircle class="mr-1 h-4 w-4 animate-spin" />
+					Please Wait
+				{:else}
+					<SaveIcon class="mr-1 h-4 w-4" />
+					Update
+				{/if}
+			</Button>
+		</div>
 	</Tooltip.Trigger>
 	<Tooltip.Content>
 		{#if needsUpdate}
-			<p>Save the current state of the form</p>
+			<p>You have unsaved changes:</p>
+			<ul>
+				{#each changedCategories as category}
+					<li>{category}</li>
+				{/each}
+			</ul>
 		{:else}
 			<p>Form is up to date</p>
 		{/if}
