870e2972201ef6fddcd8072902acd6115ac15a4d
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-11 15:22:33 +0100

    implemented bo back to Edit button in preview mode

--- src/routes/(app)/(public)/form/[id]/[viewmode]/+page.svelte
+++ src/routes/(app)/(public)/form/[id]/[viewmode]/+page.svelte
@@ -3,20 +3,29 @@
 	import ThemeWrapper from '$lib/components/custom/customizer/ThemeWrapper.svelte';
 	import Form from '$lib/components/custom/form/Form.svelte';
 	import { page } from '$app/stores';
+	import GoBackToEditButton from '$lib/components/custom/buttons/GoBackToEditButton.svelte';
 	export let data: PageServerData;
 
 	const mode = $page.params.viewmode;
 </script>
 
-<div class="flex h-screen w-screen items-center justify-center">
-	<div class="w-full max-w-[820px]">
-		<ThemeWrapper style={data.form?.style}>
-			<Form
-				items={data.form?.items}
-				info={data.form?.info}
-				isPreview={true}
-				canSubmit={mode === 'public'}
-			/>
-		</ThemeWrapper>
-	</div>
-</div>
+{#if data.form}
+	<ThemeWrapper style={data.form?.style}>
+		{#if mode === 'preview'}
+			<div class="flex justify-start">
+				<GoBackToEditButton />
+			</div>
+		{/if}
+
+		<div class="flex h-screen w-screen items-center justify-center">
+			<div class="w-full max-w-[820px]">
+				<Form
+					items={data.form?.items}
+					info={data.form?.info}
+					isPreview={true}
+					canSubmit={mode === 'public'}
+				/>
+			</div>
+		</div>
+	</ThemeWrapper>
+{/if}
