56862ef46e84e74e38bda4c0eb6fd2f2c22ccd59
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-11 15:21:36 +0100

    removed unused code

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,18 +1,4 @@
 <script lang="ts">
-	import { toast } from 'svelte-sonner';
-	import { Button } from '$lib/components/shadcn/ui/button';
-	import ErrorToast from '$lib/components/custom/ErrorToast.svelte';
-
-	function showToast() {
-		toast.custom(ErrorToast, {
-			componentProps: {
-				heading: 'Event has been created',
-				description: 'Sunday, December 03, 2023 at 9:00 AM'
-			}
-		});
-	}
 </script>
 
 <h1>Landing Page</h1>
-
-<Button on:click={() => showToast()}>Show toast</Button>
