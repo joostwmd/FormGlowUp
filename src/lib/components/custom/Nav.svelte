17f1a1b28b64e9ed6a3f8871bec0ec9829e0f54a
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-11 21:31:28 +0100

    refactor(Nav): update session handling and add logo click navigation

--- src/lib/components/custom/Nav.svelte
+++ src/lib/components/custom/Nav.svelte
@@ -1,15 +1,26 @@
-<script>
+<script lang="ts">
 	import { goto } from '$app/navigation';
 	import Button from '$lib/components/shadcn/ui/button/button.svelte';
 	import * as Avatar from '$lib/components/shadcn/ui/avatar';
+	import type { Session } from '@auth/sveltekit';
 
-	export let session;
+	export let session: Session;
+
+	async function handleLogoClick() {
+		if (session) {
+			await goto('/dashboard');
+		} else {
+			await goto('/');
+		}
+	}
 </script>
 
 <div class="flex w-full items-center justify-between border-b p-4">
-	<h1 class="text-center text-2xl font-bold">Form GlowUp</h1>
+	<Button variant="ghost" on:click={handleLogoClick}>
+		<h1 class="text-center text-2xl font-bold">Form GlowUp</h1>
+	</Button>
 
-	{#if session}
+	{#if session && session.user && session.user.name}
 		<Avatar.Root class="h-8 w-8">
 			<Avatar.Image src={session.user.image} alt="Profile Avatar" />
 			<Avatar.Fallback>{session.user.name[0]}</Avatar.Fallback>
