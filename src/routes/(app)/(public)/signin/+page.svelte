1051a1f7af8ff192bc77d9c2dc8f03ffc0ddebc4
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-11 21:31:38 +0100

    ui improvement sign in page

--- src/routes/(app)/(public)/signin/+page.svelte
+++ src/routes/(app)/(public)/signin/+page.svelte
@@ -3,8 +3,17 @@
 	import { SignIn } from '@auth/sveltekit/components';
 </script>
 
-<SignIn provider="google" signInPage="signin" class="w-full">
-	<div slot="submitButton">
-		<Button>Sign in with Google Server</Button>
+<div class="flex h-full w-full flex-col items-center justify-center space-y-4 p-4">
+	<h1 class="text-center text-4xl font-bold">Your almost there :)</h1>
+	<p class="text-center text-sm text-muted-foreground">
+		This project is in development mode, so you need to request access so i can add you to the
+		authorized users list
+	</p>
+	<div>
+		<SignIn provider="google" signInPage="signin" class="w-full">
+			<div class="flex w-full flex-col items-center justify-center" slot="submitButton">
+				<Button>Sign In with Google</Button>
+			</div>
+		</SignIn>
 	</div>
-</SignIn>
+</div>
