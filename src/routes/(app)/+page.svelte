9f292f0c286db0fb807ea44d5df3520b044664a7
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-11 21:31:43 +0100

    feat: enhance landing page with call-to-action button and session-based navigation

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,4 +1,27 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import Button from '$lib/components/shadcn/ui/button/button.svelte';
+	import type { LayoutServerData } from './$types';
+	export let data: LayoutServerData;
+
+	async function handleGetStartedClick() {
+		if (data.session) {
+			await goto('/dashboard');
+		} else {
+			await goto('/signin');
+		}
+	}
 </script>
 
-<h1>Landing Page</h1>
+<div class="flex h-full w-full flex-col items-center justify-center space-y-4 p-4">
+	<h1 class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+		10x your Google Form with a paste of a link
+	</h1>
+
+	<p class="text-center text-sm text-muted-foreground">
+		Form GlowUp is a UI Layer for your Google Forms. It will generate a modern Interface for your
+		form while mimicing almost all features
+	</p>
+
+	<Button on:click={handleGetStartedClick}>Get Started</Button>
+</div>
