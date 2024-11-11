542fd9f1f62d427ba4a85378bdaff65028446c73
joostwmd <88150245+joostwmd@users.noreply.github.com>
2024-11-11 14:12:54 +0100

    validation for gird items

--- src/lib/form/utils/validation/index.ts
+++ src/lib/form/utils/validation/index.ts
@@ -2,10 +2,11 @@ import {
 	DATE_QUESTION_ITEM,
 	TIME_QUESTION_ITEM,
 	TEXT_QUESTION_ITEM,
-	PARAGRAPH_QUESTION_ITEM
+	PARAGRAPH_QUESTION_ITEM,
+	GRID_ITEM_TYPES
 } from '$lib/form/constants';
 import type { TFormDataStore } from '$lib/form/stores';
-import type { TDateItem, TTimeItem, TTextItem, TFormItem } from '$lib/form/types';
+import type { TDateItem, TTimeItem, TTextItem, TFormItem, TGridItem } from '$lib/form/types';
 import { validateCustomParameters } from './custom';
 import { validateDate } from './date';
 import { validateTime } from './time';
@@ -14,22 +15,39 @@ export function validateFormItemData(
 	item: TFormItem,
 	formData: TFormDataStore
 ): { valid: boolean; message: string } {
-	const value = formData[item.submitId] as string;
+	// Validate grid item types
+	if (GRID_ITEM_TYPES.includes(item.type) && item.validation.isRequired) {
+		const gridItem = item as TGridItem;
+		const submitIds = gridItem.rows.map((row) => row.submitId);
+		const values = submitIds.map((id) => formData[id] as string);
+		const hasEmptyValues = values.filter((value) => value === '' || value === undefined).length > 0;
+		if (hasEmptyValues) {
+			return { valid: false, message: 'Every row is required' };
+		} else {
+			return { valid: true, message: '' };
+		}
+	} else {
+		// Validate required fields
+		const value = formData[item.submitId] as string;
+		if (item.validation.isRequired && (value === '' || value === undefined)) {
+			return { valid: false, message: 'This field is required' };
+		}
 
-	if (item.validation.isRequired && (value === '' || value === undefined)) {
-		return { valid: false, message: 'This field is required' };
+		// Validate date question item
+		if (item.type === DATE_QUESTION_ITEM) {
+			return validateDate(item as TDateItem, formData);
+		}
+
+		// Validate time question item
+		if (item.type === TIME_QUESTION_ITEM) {
+			return validateTime(item as TTimeItem, formData);
+		}
+
+		// Validate text and paragraph question items
+		if (TEXT_QUESTION_ITEM.includes(item.type) || PARAGRAPH_QUESTION_ITEM.includes(item.type)) {
+			return validateCustomParameters(item as TTextItem, value);
+		}
+
+		return { valid: true, message: '' };
 	}
-
-	if (item.type === DATE_QUESTION_ITEM) {
-		return validateDate(item as TDateItem, formData);
-	} else if (item.type === TIME_QUESTION_ITEM) {
-		return validateTime(item as TTimeItem, formData);
-	} else if (
-		TEXT_QUESTION_ITEM.includes(item.type) ||
-		PARAGRAPH_QUESTION_ITEM.includes(item.type)
-	) {
-		return validateCustomParameters(item as TTextItem, value);
-	}
-
-	return { valid: true, message: '' };
 }
